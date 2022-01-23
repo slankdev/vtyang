2514b0506df80123d1f5dc0edbfa5f6a9b0a8e81
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-01-23 16:26:28 +0900

    2022.01.23-16:26

--- pkg/vtyang/command.go
+++ pkg/vtyang/command.go
@@ -1,11 +1,8 @@
 package vtyang
 
 import (
-	"fmt"
 	"os"
-	"sort"
 
-	"github.com/openconfig/goyang/pkg/yang"
 	"github.com/spf13/cobra"
 )
 
@@ -16,8 +13,7 @@ var config = struct {
 
 func NewCommand() *cobra.Command {
 	rootCmd := &cobra.Command{
-		Use:  "vtyang",
-		RunE: main,
+		Use: "vtyang",
 	}
 
 	fs := rootCmd.PersistentFlags()
@@ -25,6 +21,7 @@ func NewCommand() *cobra.Command {
 	fs.StringArrayVarP(&config.GlobalOptPaths, "path", "p", []string{}, "Module paths")
 	rootCmd.AddCommand(newCommandCompletion(rootCmd))
 	rootCmd.AddCommand(newCommandAgent())
+	rootCmd.AddCommand(newCommandDump())
 	return rootCmd
 }
 
@@ -32,6 +29,14 @@ func newCommandAgent() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "agent",
 		RunE: agentMain,
+	}
+	return cmd
+}
+
+func newCommandDump() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:  "dump",
+		RunE: dumpMain,
 	}
 	return cmd
 }
@@ -64,46 +69,4 @@ func newCommandCompletion(rootCmd *cobra.Command) *cobra.Command {
 	})
 
 	return cmd
-}
-
-func main(cmd *cobra.Command, args []string) error {
-	files := []string{
-		"./yang/model1.yang",
-		"./yang/model2.yang",
-	}
-	modules := yang.NewModules()
-	for _, name := range files {
-		if err := modules.Read(name); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-	}
-
-	errs := modules.Process()
-	if len(errs) > 0 {
-		for _, err := range errs {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		return errs[0]
-	}
-
-	mods := map[string]*yang.Module{}
-	var names []string
-
-	for _, m := range modules.Modules {
-		if mods[m.Name] == nil {
-			mods[m.Name] = m
-			names = append(names, m.Name)
-		}
-	}
-	sort.Strings(names)
-	entries := make([]*yang.Entry, len(names))
-	for x, n := range names {
-		entries[x] = yang.ToEntry(mods[n])
-	}
-
-	for _, e := range entries {
-		write(os.Stdout, e)
-	}
-	return nil
 }

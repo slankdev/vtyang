d3a07a1af814abfbb3a9ce4ae94357863e1066dc
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2024-04-13 10:04:52 +0900

    2024.04.13-10:04

--- cmd/frr-agent/sub.mk
+++ cmd/frr-agent/sub.mk
@@ -1,6 +1,6 @@
 frr_agent_img := slankdev/frr-agent:develop
 frr-agent-build:
-	CGO_ENABLED=0 go build -o bin/frr-agent cmd/frr-agent/main.go
+	GOOS=linux CGO_ENABLED=0 go build -o bin/frr-agent cmd/frr-agent/main.go
 frr-agent-docker-build: ## Build docker image with the frr-agent.
 	docker build -t ${frr_agent_img} -f cmd/frr-agent/Dockerfile .
 frr-agent-docker-push: frr-agent-docker-build ## Push docker image with the frr-agent.

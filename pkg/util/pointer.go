c824f2e79a861d66f4c5a00f297641b4dc0fa1d2
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2024-04-22 00:17:20 +0900

    2024.04.22-00:17

---
+++ pkg/util/pointer.go
@@ -0,0 +1,13 @@
+package util
+
+func NewBoolPointer(b bool) *bool {
+	return &b
+}
+
+func NewUint64Pointer(v uint64) *uint64 {
+	return &v
+}
+
+func NewInt64Pointer(v int64) *int64 {
+	return &v
+}

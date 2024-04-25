727106e7530c5adb5a132b110d5562881d014e82
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2024-04-26 08:31:47 +0900

    2024.04.26-08:31

--- pkg/vtyang/agent_test.go
+++ pkg/vtyang/agent_test.go
@@ -1,10 +1,7 @@
 package vtyang
 
 import (
-	"os"
 	"testing"
-
-	"github.com/slankdev/vtyang/pkg/util"
 )
 
 const (
@@ -49,83 +46,22 @@ func TestAgentLoadDatabase(t *testing.T) {
 	})
 }
 
-const TestAgentNoDatabaseOutput3 = `{
-  "isis": {
-    "instance": [
-      {
-        "area-address": [
-          "10.0000.0000.0000.0000.0000.0000.0000.0000.0000.00",
-          "20.0000.0000.0000.0000.0000.0000.0000.0000.0000.00"
-        ],
-        "area-tag": "1",
-        "description": "area1-default-hoge",
-        "vrf": "default"
-      },
-      {
-        "area-tag": "1",
-        "description": "area1-vrf0-fuga",
-        "vrf": "vrf0"
-      },
-      {
-        "area-tag": "2",
-        "description": "area2-vrf0-hoge",
-        "vrf": "vrf0"
-      }
-    ]
-  }
-}
-`
-
 func TestAgentXPathCli(t *testing.T) {
-	testcases := []TestCaseForTestAgent{
-		{
-			Inputs: []string{
-				"configure",
-				"set isis instance 1 default description area1-default-hoge",
-				"set isis instance 1 vrf0 description area1-vrf0-hoge",
-				"set isis instance 2 vrf0 description area2-vrf0-hoge",
-				"set isis instance 1 vrf0 description area1-vrf0-fuga",
-				"set isis instance 1 default area-address 10.0000.0000.0000.0000.0000.0000.0000.0000.0000.00 20.0000.0000.0000.0000.0000.0000.0000.0000.0000.00",
-				"commit",
-				"do show running-config",
-			},
-			Output: TestAgentNoDatabaseOutput3,
+	executeTestCase(t, &TestCase{
+		YangPath:    "../../yang.frr/",
+		RuntimePath: "/tmp/run/vtyang",
+		OutputFile:  "./testdata/xpath_cli/output.txt",
+		Inputs: []string{
+			"configure",
+			"set isis instance 1 default description area1-default-hoge",
+			"set isis instance 1 vrf0 description area1-vrf0-hoge",
+			"set isis instance 2 vrf0 description area2-vrf0-hoge",
+			"set isis instance 1 vrf0 description area1-vrf0-fuga",
+			"set isis instance 1 default area-address 10.0000.0000.0000.0000.0000.0000.0000.0000.0000.00 20.0000.0000.0000.0000.0000.0000.0000.0000.0000.00",
+			"commit",
+			"do show running-config",
 		},
-	}
-
-	// Preparation
-	GlobalOptRunFilePath = RUNTIME_PATH
-	if util.FileExists(getDatabasePath()) {
-		if err := os.Remove(getDatabasePath()); err != nil {
-			t.Error(err)
-		}
-	}
-
-	// Initializing Agent
-	if err := InitAgent(RUNTIME_PATH,
-		"../../yang.frr/"); err != nil {
-		t.Fatal(err)
-	}
-
-	// EXECUTE TEST CASES
-	for idx, tc := range testcases {
-		buf := setStdoutWithBuffer()
-		for _, input := range tc.Inputs {
-			t.Logf("Testcase[%d] executing %s", idx, input)
-			getCommandNodeCurrent().executeCommand(input)
-		}
-		result := buf.String()
-		if tc.Output != result {
-			t.Errorf("Unexpected output")
-			for _, input := range tc.Inputs {
-				t.Errorf("input %+v", input)
-			}
-			t.Errorf("expect(len=%d) %+v", len(tc.Output), tc.Output)
-			t.Errorf("result(len=%d) %+v", len(result), result)
-			t.Fatal("quiting test with FAILED result")
-		}
-		t.Logf("Testcase[%d] output check is succeeded", idx)
-	}
+	})
 }
 
 func TestAgentXPathCliFRR(t *testing.T) {
